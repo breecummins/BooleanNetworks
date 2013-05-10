15fc576a51679bf3d5450a775290f18fe13cc4ff
Bree Cummins <breecummins@gmail.com>
2013-05-10 12:35:39 -0600

    wrote functions to try random initial conditions

--- BooleanNetworks/networkScripts.py
+++ BooleanNetworks/networkScripts.py
@@ -36,6 +36,37 @@ def alterParams(maindir,Alist=[0.5,1.0,1.5,2.0],Blist=[-0.5,-1.0,-2.0],per=[0.0,
             fname4=os.path.join(maindir,'model4tracksA'+str(k)+'B'+str(j))
             partitionOrthant(mod4,fname4)
 
+def partitionOrthantRandInits(inits,model=mN.model1,fname=os.path.expanduser('~/temp/model1tracks'),dt=0.01,finaltime=5.0):    
+    tracks = []
+    inik = np.ones(5)
+    for k in range(inits.shape[0]):
+        inik[1:] = inits[k,:]
+        ts = mN.solveModel(inik,finaltime,model,dt)
+        tracks.append(mN.translateToOrthants(ts))
+    fname += '.pickle'
+    cPickle.dump(tracks,open(fname,'w'))
+
+def randInits(maindir,Alist=[0.5,1.0,1.5,2.0],Blist=[-0.5,-1.0,-2.0],dt=0.01):
+    inits = -2.1 + 2*np.random.random((2000,4))
+    cPickle.dump({'inits':inits},open(os.path.join(maindir,'inits'),'w'))
+    for k,A in enumerate(Alist):
+        L0 = partial(mN.L0,A0=A)
+        fname1=os.path.join(maindir,'model1tracksA'+str(k))
+        mod1 = partial(mN.model1,L0=L0)
+        partitionOrthantRandInits(inits,mod1,fname1,dt=dt)
+        for j,B in enumerate(Blist):
+            L4 =partial(mN.L0,A0=1.0,B0=B,C0=2.0,D0=1.0)
+            fname2=os.path.join(maindir,'model2tracksA'+str(k)+'B'+str(j))
+            mod2=partial(mN.model2,L0=L0,L4=L4)
+            partitionOrthantRandInits(inits,mod2,fname2)
+            mod3=partial(mN.model3,L0=L0,L4=L4)
+            fname3=os.path.join(maindir,'model3tracksA'+str(k)+'B'+str(j))
+            partitionOrthantRandInits(inits,mod3,fname3)
+            newL4=partial(mN.L4,B0=B)
+            mod4=partial(mN.model4,L0=L0,L4=newL4)
+            fname4=os.path.join(maindir,'model4tracksA'+str(k)+'B'+str(j))
+            partitionOrthantRandInits(inits,mod4,fname4)
+
 def sortTracks(lot):
     numtracks = len(lot)
     uniqtracks = list(set([tuple(l.flatten()) for l in lot]))
---
+++ BooleanNetworks/script3.py
@@ -0,0 +1,6 @@
+#!/usr/bash python
+
+import networkScripts as nS
+import os
+
+nS.randInits(os.path.expanduser('~/temp/dataset_randinits/'),dt=0.001)
