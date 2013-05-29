5079fe395a1d0db29c943f63976d53d902f02949
Bree Cummins <breecummins@gmail.com>
2013-05-29 14:49:51 -0600

    wrote script to run rand inits with x varying between 1 and 10. Also altered batch code to save data as ints rather than boolean arrays.

--- BooleanNetworks/networkScripts.py
+++ BooleanNetworks/networkScripts.py
@@ -39,13 +39,12 @@ def alterParams(maindir,Alist=[0.5,1.0,1.5,2.0],Blist=[-0.5,-1.0,-2.0],per=[0.0,
             fname4=os.path.join(maindir,'model4tracksA'+str(k)+'B'+str(j))
             partitionOrthant(mod4,fname4)
 
-def partitionOrthantRandInits(xinit,inits,model=mN.model1,fname=os.path.expanduser('~/temp/model1tracks'),dt=0.01,finaltime=5.0):    
+def partitionOrthantRandInits(inits,model=mN.model1,fname=os.path.expanduser('~/temp/model1tracks'),dt=0.01,finaltime=5.0):    
     tracks = []
-    inik = xinit*np.ones(5)
     for k in range(inits.shape[0]):
-        inik[1:] = inits[k,:]
-        ts = mN.solveModel(inik,finaltime,model,dt)
-        tracks.append(mN.translateToOrthants(ts))
+        ts = mN.solveModel(inits[k,:],finaltime,model,dt)
+        t = mN.translateToOrthants(ts)
+        tracks.append(mN.encodeInts(t))
     fname += '.pickle'
     of = open(fname,'w')
     cPickle.dump(tracks,of)
@@ -64,27 +63,66 @@ def randInits(maindir,Alist=[0.5,1.0,1.5,2.0],Blist=[-0.5,-1.0,-2.0],dt=0.01,xin
         idict=cPickle.load(of)
         of.close()
         inits=idict['inits']
+    initsx = xinit*np.ones(5)
+    initsx[:,1:] = inits
+    del(inits)
+    inits = initsx
     for k,A in enumerate(Alist):
         print('Model 1, A = ' + str(A))
         L0 = partial(mN.L0,A0=A)
         fname1=os.path.join(maindir,'model1tracksA'+str(k))
         mod1 = partial(mN.model1,L0=L0)
-        partitionOrthantRandInits(xinit,inits,mod1,fname1,dt=dt)
+        partitionOrthantRandInits(inits,mod1,fname1,dt=dt)
         for j,B in enumerate(Blist):
             print('Model 2, A = ' + str(A) + ', B = ' + str(B))
             L4 =partial(mN.L0,A0=1.0,B0=B,C0=2.0,D0=1.0)
             fname2=os.path.join(maindir,'model2tracksA'+str(k)+'B'+str(j))
             mod2=partial(mN.model2,L0=L0,L4=L4)
-            partitionOrthantRandInits(xinit,inits,mod2,fname2)
+            partitionOrthantRandInits(inits,mod2,fname2)
             print('Model 3, A = ' + str(A) + ', B = ' + str(B))
             mod3=partial(mN.model3,L0=L0,L4=L4)
             fname3=os.path.join(maindir,'model3tracksA'+str(k)+'B'+str(j))
-            partitionOrthantRandInits(xinit,inits,mod3,fname3)
+            partitionOrthantRandInits(inits,mod3,fname3)
             newL4=partial(mN.L4,B0=B)
             print('Model 4, A = ' + str(A) + ', B = ' + str(B))
             mod4=partial(mN.model4,L0=L0,L4=newL4)
             fname4=os.path.join(maindir,'model4tracksA'+str(k)+'B'+str(j))
-            partitionOrthantRandInits(xinit,inits,mod4,fname4)
+            partitionOrthantRandInits(inits,mod4,fname4)
+
+def randInitsWithX(maindir,Alist=[0.5,1.0,1.5,2.0],Blist=[-0.5,-1.0,-2.0],dt=0.01,numinits=20000):
+    if 'inits.pickle' not in os.listdir(maindir):
+        print('Generating new initial conditions...')
+        inits = -2.1 + 2*np.random.random((numinits,5))
+        inits[:,0] = 10.0 - 9*np.random.random((numinits,))
+        of = open(os.path.join(maindir,'inits.pickle'),'w')
+        cPickle.dump(inits,of)
+        of.close()
+    else:
+        print('Using existing initial conditions file...')
+        of = open(os.path.join(maindir,'inits.pickle'),'r')
+        inits=cPickle.load(of)
+        of.close()
+    for k,A in enumerate(Alist):
+        print('Model 1, A = ' + str(A))
+        L0 = partial(mN.L0,A0=A)
+        fname1=os.path.join(maindir,'model1tracksA'+str(k))
+        mod1 = partial(mN.model1,L0=L0)
+        partitionOrthantRandInits(inits,mod1,fname1,dt=dt)
+        for j,B in enumerate(Blist):
+            print('Model 2, A = ' + str(A) + ', B = ' + str(B))
+            L4 =partial(mN.L0,A0=1.0,B0=B,C0=2.0,D0=1.0)
+            fname2=os.path.join(maindir,'model2tracksA'+str(k)+'B'+str(j))
+            mod2=partial(mN.model2,L0=L0,L4=L4)
+            partitionOrthantRandInits(inits,mod2,fname2)
+            print('Model 3, A = ' + str(A) + ', B = ' + str(B))
+            mod3=partial(mN.model3,L0=L0,L4=L4)
+            fname3=os.path.join(maindir,'model3tracksA'+str(k)+'B'+str(j))
+            partitionOrthantRandInits(inits,mod3,fname3)
+            newL4=partial(mN.L4,B0=B)
+            print('Model 4, A = ' + str(A) + ', B = ' + str(B))
+            mod4=partial(mN.model4,L0=L0,L4=newL4)
+            fname4=os.path.join(maindir,'model4tracksA'+str(k)+'B'+str(j))
+            partitionOrthantRandInits(inits,mod4,fname4)
 
 if __name__ == "__main__":
         alterParams()
---
+++ BooleanNetworks/script4.py
@@ -0,0 +1,6 @@
+#!/usr/bash python
+
+import networkScripts as nS
+import os
+
+nS.randInitsWithX(os.path.expanduser('~/temp/dataset_randinits_x1to10/'),dt=0.01)
