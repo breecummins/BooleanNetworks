a67283abc336e7d2d326a09d681678be0cb7dd65
Bree Cummins <breecummins@gmail.com>
2013-06-14 16:19:07 -0600

    guts working, framework done except for finding and printing constraints

--- BooleanNetworks/directedGraphsToMaps.py
+++ BooleanNetworks/directedGraphsToMaps.py
@@ -53,9 +53,7 @@ def makeMap(acts,reps):
     alldeps = acts+reps
     allcombos = itertools.chain(*[itertools.combinations(alldeps,n) for n in range(len(alldeps)+1)])
     nodestates = []
-    nodeinds = []
     for c in allcombos:
-        nodeinds.append(c)
         ns = [0]*len(acts) + [1]*len(reps)
         for j in c:
             if j in acts:
@@ -66,58 +64,78 @@ def makeMap(acts,reps):
     nodestateints = [int(''.join([str(a) for a in n]),2) for n in nodestates]
 
     def recurseMaps(maps,inds,templ):
+        # start by taking combinations of 0's and 1's for the given state indices
         for p in itertools.product([1,0],repeat=len(inds)):
             tp = list(templ)
             for k,j in enumerate(inds):
                 tp[j] = p[k]
             ons = [s for i,s in enumerate(inds) if p[i] == 1]
+            # propagate the 'ons' forward; i.e., if (0,0,1) maps to 1, then everything 
+            # else with (0,0,1) as a subset (e.g. (1,0,1)) must map to 1 as well
             for k1,t in enumerate(tp):
                 if t == None and any([nodestateints[k1] & nodestateints[s] == nodestateints[s] for s in ons]):
                     tp[k1] = 1
             nonecount = tp.count(None)
             if nonecount == 0:
+                # if the map is complete, record it and continue
                 maps.append(tp) 
             elif nonecount == 1:
-                tp0 = list(tp)
-                tp1 = list(tp)
+                # if there are two possible maps, record them and continue
                 ind = tp.index(None)
-                tp0[ind]=0
-                tp1[ind]=1
-                maps.append(tp0)
-                maps.append(tp1)
+                tp[ind]=0
+                maps.append(tp)
+                tp[ind]=1
+                maps.append(tp)
             else:
-                noneinds = [j for j in range(len(tp)) if tp[j]==None]
-                minsums = min([sum(nodestates[j]) for j in noneinds])
-                minsuminds = [j for j in noneinds if sum(nodestates[j]) == minsums]
+                # There could be 4 or more possible maps, have to check. 
+                # Find the indices of the smallest number of 'on' activators + 'off' repressors
+                # and run the algorithm again.
+                noneinds = [(j,sum(nodestates[j])) for j in range(len(tp)) if tp[j]==None]
+                minsums = min([n[1] for n in noneinds])
+                minsuminds = [n[0] for n in noneinds if n[1] == minsums]
                 maps = recurseMaps(maps,minsuminds,tp)
         return maps
-
 
     template = [None]*len(nodestates)
     template[nodestateints.index(0)] = 0
     template[nodestateints.index(2**len(alldeps)-1)] = 1
-    # find indices of single values
-    singleinds = [j for j in range(len(nodestates)) if sum(nodestates[j])==1]
-    if len(singleinds) != len(alldeps):
-        raise ValueError('There is a logic bug here. Squish it.')
+    if len(nodestates) > 2:
+        # find indices of single positive values ('on' activator or 'off' repressor)
+        singleinds = [j for j in range(len(nodestates)) if sum(nodestates[j])==1]
+        maps = recurseMaps([],singleinds,template)
+    else:
+        maps = [template]
+    return alldeps, nodestates, maps
 
-    maps = recurseMaps([],singleinds,template)
+def getConstraints(alldeps,nodestates,maps):
+    return None
 
-    print(nodeinds)
-    print(nodestates)
-    print(maps)
-    # need to assign 1 or 0 for each nodestate consistent with Hill function framework
-    # return alldeps, nodestates, maps
-
-def makeAllMaps(nodes,activatorinds,repressorinds):
-    pass
-    # call makeMap inside for loop
-
-def getConstraints():
-    pass
-
-def prettyOutput():
-    pass
+def prettyOutput(nodes,alldeps,nodestates,maps):
+    deps = tuple([nodes[d] for d in alldeps])
+    print("{0}".format(deps).replace("'", "")), 
+    print('maps:'),
+    L = len(str(deps).replace("'", "") + ' maps:')
+    l = len(str(nodestates[0]))
+    for k in range(1,len(maps)+1):
+        if k < len(maps) and k < 9:
+            print('{0} '.format(k)),
+        elif k < len(maps) and k >= 9:
+            print(k),
+        else:
+            print(k)
+    print('-'*((L+3*len(maps))))
+    for k,n in enumerate(nodestates):
+        print(n),
+        for i in range(len(maps)):
+            if i == 0 and i < len(maps)-1:
+                print(' '*(L-l) +'{0}'.format(maps[i][k])),
+            elif i == 0 and i == len(maps)-1:
+                print(' '*(L-l) +'{0}'.format(maps[i][k]))
+            elif i < len(maps)-1:
+                print(' {0}'.format(maps[i][k])),
+            else:
+                print(' {0}'.format(maps[i][k]))
+    print('-'*((L+3*len(maps))))
 
 def translateNetworkToMaps(listofedges):
     '''
@@ -135,6 +153,15 @@ def translateNetworkToMaps(listofedges):
 
     '''
     nodes,activatorinds,repressorinds = nodesAndSigns(listofedges)
-    maps,nodeinds = makeAllMaps(nodes,activatorinds,repressorinds)
-    constraints = getConstraints()
-    prettyOutput(nodes,nodeinds,maps,constraints)
+    nodedeps = []
+    nodedepstates = []
+    nodemaps = []
+    for k,n in enumerate(nodes):
+        alldeps, nodestates, maps = makeMap(activatorinds[k],repressorinds[k])
+        constraints = getConstraints(alldeps,nodestates,maps)
+        print('Node {0}'.format(n))
+        prettyOutput(nodes,alldeps,nodestates,maps)
+        nodedeps.append(alldeps)
+        nodedepstates.append(nodestates)
+        nodemaps.append(maps)
+    return nodes,nodedeps,nodedepstates,nodemaps
