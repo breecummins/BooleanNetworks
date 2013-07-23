9bd5ff2afd99c673ef3140b6add09829a5fa72cc
Bree Cummins <breecummins@gmail.com>
2013-07-23 16:31:34 -0600

    hyperplane discretization seems to be working, but I only spot-checked. further testing needed.

--- BooleanNetworks/VectorizedBooleanMapForDB.py
+++ BooleanNetworks/VectorizedBooleanMapForDB.py
@@ -1,5 +1,6 @@
 import numpy as np
 import BooleanMapForDB as BDB
+import itertools
 
 def makeHyperplaneGrids(res,thresh,maxvals):
     '''
@@ -54,17 +55,24 @@ def makeHyperplaneGrids(res,thresh,maxvals):
             cu.append( np.arange(m + slop[j]/2 + res/2, maxs[j], res) )
         chunks.append( cu )
     # assemble chunks into hyperplanes
+    indices = [range(len(_c)) for _c in chunks]
     hyperplanes = []
     for k,u in enumerate(uniqthresh):
-        for j,xt in enumerate(u):
-            #make combinations for i != k of the chunks and combine with np.array([xt]) to get hyperplane
-            pass
-            # for i in range(len(chunks)):
-            #     if i != k: 
-            #         for c in chunks[i]:
-                        
-
-
+        # excise var at threshold
+        lessk = range(N)
+        lessk.remove(k)
+        subinds = indices[:k] + indices[k+1:]
+        # make all combinations of remaining chunks
+        for p in itertools.product(*subinds):
+            hp = [0]*N
+            for j,i in enumerate(p):
+                hp[lessk[j]] = chunks[lessk[j]][i]
+            # combine with threshold values into hyperplane discretizations
+            for xt in u:
+                hpxt = list(hp)
+                hpxt[k] = np.array([xt])
+                hyperplanes.append( hpxt )                        
+    return hyperplanes
 
 def subdivideBox(N,minval,maxval):
     pass
@@ -101,3 +109,17 @@ def makeModel(sources,targets,thresholds,amplitudes,decayrates,repressors):
     dr = np.array(decayrates)
     thresh,amp,rep = BDB.makeParameterArrays(sources,targets,thresholds,amplitudes,repressors)
     return thresh,amp,rep,dr
+
+if __name__ == '__main__':
+    sources = ['x','y1','y2','z']
+    targets = [['x','y1','z'],['y2'],['x','z'],['x']]
+    thresholds = [[0.25,0.5,0.75],[0.5],[0.5,0.5],[0.5]]
+    amplitudes = [[0.5,1.0,1.0],[1.0],[1.0,1.0],[1.0]]
+    decayrates = [1.0,0.5,0.5,0.5]
+    repressors = [('z','x')]
+    thresh,amp,rep,dr = makeModel(sources,targets,thresholds,amplitudes,decayrates,repressors)
+    res = 0.1
+    maxvals = [1.0]*len(sources)
+    hyperplanes = makeHyperplaneGrids(res,thresh,maxvals)
+    print(len(hyperplanes))
+    print(hyperplanes)
