f82c5480f4dc7006ba441655d8820b1955b7859e
Bree Cummins <breecummins@gmail.com>
2013-08-06 11:02:43 -0600

    debugging vectorized code (problem with focal points). added production rates. still need to add steady vertices.

--- BooleanNetworks/VectorizedBooleanMapForDB.py
+++ BooleanNetworks/VectorizedBooleanMapForDB.py
@@ -1,20 +1,13 @@
 import numpy as np
-import BooleanMapForDB as BDB
 import itertools
 
-def makeHyperplanes(thresh,maxvals,eps=0.0):
+def makeWalls(thresh,maxvals):
     '''
     Construct all hyperplanes, where xi equals one of its 
     threshold values and no other xj is at threshold. 
     Assume N variables total. Then each hyperplane will be 
     rectangular in N-1 dimensional space with side lengths 
     w1 by w2 by ... by wi-1 by wi+1 by ... by wN. 
-
-    Make a compact box inside each hyperplane that is bounded 
-    away from the threshold values at the boundaries by eps*wj,
-    where eps is a user-supplied value between 0 and 1,
-    usually very small, and wj is the length of the side of
-    the box associated with the jth variable.
 
     thresh is the NxN parameter array containing the thresholds.
     maxvals is a user-supplied list of length N of the maximum 
@@ -53,9 +46,6 @@ def makeHyperplanes(thresh,maxvals,eps=0.0):
     for k,u in enumerate(uniqthresh):
         mins = np.concatenate((np.array([0]), u))
         maxs = np.concatenate((u, np.array([maxvals[k]])))
-        lens = maxs - mins
-        mins = mins + eps*lens
-        maxs = maxs - eps*lens
         chunks.append( [(m,maxs[k]) for k,m in enumerate(mins)])
     # assemble chunks into hyperplane boxes
     indices = [range(len(_c)) for _c in chunks]
@@ -77,24 +67,7 @@ def makeHyperplanes(thresh,maxvals,eps=0.0):
                 hyperplanes.append( hpxt )                        
     return hyperplanes
 
-def constructBoxes(hyperplanes):
-    hpboxes = []
-    for hp in hyperplanes:
-        ind = [h[0]==h[1] for h in hp].index(1) #find which var is at a thresh
-        subinds = range(len(hp))
-        subinds.remove(ind)
-        vertices = []
-        for p in itertools.product([0,1],repeat=len(subinds)):
-            pt = np.empty(len(subinds)+1)
-            pt.fill(np.nan) #much faster than np.nan*np.ones(len(subinds)+1), using nan's to make bugs easier to catch
-            pt[ind] = hp[ind][0] #fill in threshold value
-            for i,k in enumerate(p):
-                pt[i] = hp[i][k] #fill either max or min for every other var in the hyperplane
-            vertices.append(pt) 
-        hpboxes.append(vertices) #save all pts in one hyperplane into one box
-    return hpboxes
-
-def getDomainsAndFocalPoints(thresh,amp,rep,dr,maxvals):
+def getDomainsAndFocalPoints(thresh,amp,rep,dr,pr,maxvals):
     # for each source, collect all the halfway points 
     # between thresholds, including below the first and 
     # above the last thresholds
@@ -107,11 +80,11 @@ def getDomainsAndFocalPoints(thresh,amp,rep,dr,maxvals):
     # find the midpoints of each domain
     doms = np.array(list(itertools.product(*dp)))
     # get the focal point for the midpoint of each domain
-    # FIXME: add constant production rates
     fps = []
-    prev = np.zeros(thresh[0,:].shape)
     for i in range(doms.shape[0]):
-        fps.append( BDB.getFocalPoint(doms[i,:],thresh,amp,rep,dr,prev) )
+        up = (doms[i,:] > thresh)  #this is focal point code from BooleanMapForDB 
+        A = amp*(up ^ rep) # without look-ahead because we're in a regular domain   
+        fps.append( (A.sum(1) + pr)/dr ) # with the addition of constant production rates
     # return the midpoints of each domain and the focal points for the domains
     return doms, fps
 
@@ -123,7 +96,7 @@ def identifyWhiteWalls(hyperplanes,doms,fps,dr):
     # If not, identify if there is a white wall. (Can't handle black
     # walls yet.)
     whitewalls = []
-    unidirectional = []
+    unidirwalls = []
     unidirfps = []
     for h in hyperplanes:
         # shift indices by 1 since 0 will mean failure
@@ -159,30 +132,25 @@ def identifyWhiteWalls(hyperplanes,doms,fps,dr):
         loflow = -dr[hind]*th + lofp[hind]
         hiflow = -dr[hind]*th + hifp[hind]
         if loflow < 0 and hiflow <= 0:
-            #flow is unidirectional toward lower domain
-            unidirectional.append( h )
+            #flow is unidirwalls toward lower domain
+            unidirwalls.append( h )
             unidirfps.append( lofp )
         elif loflow >= 0 and hiflow > 0:
-            #flow is unidirectional toward upper domain
-            unidirectional.append( h )
+            #flow is unidirwalls toward upper domain
+            unidirwalls.append( h )
             unidirfps.append( hifp )
         elif loflow < 0 and hiflow > 0:
             #we have a white wall
             whitewalls.append( h )
-        # elif loflow == 0 and hiflow == 0:
-        #     # We have a black wall and the flow will never leave.
-        #     blackwalls.append( h )
-        #     # Assign the focal point/steady point to be the average of the focal points on either side
-        #     blackfps.append( np.mean([lofp,hifp]) )
         else:
             raise ValueError("FIXME: The parameters of the system returned a black wall and we can't handle that yet.")
-    return unidirectional, unidirfps, whitewalls  
+    return unidirwalls, unidirfps, whitewalls  
 
-def getNextThresholds(unidirectional, unidirfps, thresh):
+def getNextThresholds(unidirwalls, unidirfps, thresh):
     next_threshs = []
     for k,fp in enumerate(unidirfps):
         # get possible thresholds
-        h = unidirectional[k]
+        h = unidirwalls[k]
         hpt = [np.mean(p) for p in h]
         pt = ( (hpt-thresh)*(fp-thresh) < 0 )*thresh
         # get the closest threshold to each coordinate
@@ -195,15 +163,33 @@ def getNextThresholds(unidirectional, unidirfps, thresh):
         next_threshs.append( nt )
     return next_threshs
 
+def constructVertices(unidirwalls,eps=0.001):
+    '''
+    Make a compact box inside each hyperplane that is bounded 
+    away from the threshold values at the boundaries by eps*wj,
+    where eps is a user-supplied value between 0 and 1,
+    usually very small, and wj is the length of the side of
+    the box associated with the jth variable.
 
-def getMinsMaxs(mappedpts,thresh):
-    pass
-
-def outerApprox(mins,maxs,boxes):
-    pass
-
-def subdivideBoxes(boxes):
-    pass
+    '''
+    wallvertices = []
+    for hp in unidirwalls:
+        ind = [h[0]==h[1] for h in hp].index(1) #find which var is at a thresh
+        subinds = range(len(hp))
+        subinds.remove(ind)
+        lens = []
+        for s in subinds:
+            lens.append( hp[s][1] - hp[s][0] )
+        vertices = []
+        for p in itertools.product([0,1],repeat=len(subinds)):
+            pt = np.empty(len(subinds)+1)
+            pt.fill(np.nan) #much faster than np.nan*np.ones(len(subinds)+1), using nan's to make bugs easier to catch
+            pt[ind] = hp[ind][0] #fill in threshold value
+            for i,k in enumerate(p):
+                pt[subinds[i]] = hp[subinds[i]][k] + lens[i]*eps*((-1)**(k)) #fill either max or min adjusted by eps for every other var in the wall
+            vertices.append(pt) 
+        wallvertices.append(vertices) #save all pts in one hyperplane into one box
+    return wallvertices
 
 def getTraversalTimes(init,fp,next_threshs,dr):
     '''
@@ -221,6 +207,10 @@ def getTraversalTimes(init,fp,next_threshs,dr):
 
     '''
     expminusT = []
+    print(init)
+    print(fp)
+    print(next_threshs)
+    print("")
     for k,i in enumerate(init):
         xT = next_threshs[k]
         if xT > 0:
@@ -229,15 +219,16 @@ def getTraversalTimes(init,fp,next_threshs,dr):
 
 def mapOnePointToMultipleHyperplanes(pt,fp,next_threshs,dr):
     expminusT = getTraversalTimes(pt,fp,next_threshs,dr)
+    print(expminusT)
+    print("")
     allnextsteps = []
     for eT in expminusT:
         allnextsteps.append( fp + (pt-fp)*(eT**dr) )
-    ind = np.nonzero(expminusT == min(expminusT))[0]
+    ind = np.nonzero(expminusT == max(expminusT))[0]
     shorteststep = allnextsteps[ind]
     return shorteststep, allnextsteps
 
-
-def mapManyPointsToMultipleHyperplanes(vertices,thresh,amp,rep,dr,previous):
+def mapManyPointsToMultipleHyperplanes(vertices,next_threshs,focalpts,dr):
     '''
     vertices is a list of numpy arrays of points in 
     N-dimensional space denoting current location.
@@ -257,34 +248,58 @@ def mapManyPointsToMultipleHyperplanes(vertices,thresh,amp,rep,dr,previous):
 
     '''
     mappedpts = []
+    mappedptsallsteps = []
     for k,v in enumerate(vertices):
-        mappedpts.append( BDB.takeAStep(v,thresh,amp,rep,dr,previous[k]) )
-    return mappedpts
+        s,a = mapOnePointToMultipleHyperplanes(v,focalpts[k],next_threshs[k],dr)
+        mappedpts.append( s )
+        mappedptsallsteps.append( a )
+    return mappedpts, mappedptsallsteps
 
-def runModel(thresh,amp,rep,dr,maxvals):
-    hp = makeHyperplanes(thresh,maxvals)
-    initboxes = constructBoxes(hp)
-    s = initboxes[0][0].shape
-    previous1 = [np.zeros(s) for _ in range(len(initboxes[0]))] 
-    M = max(maxvals)
-    previous2 = [p+M for p in previous1] 
-    boxes = []
-    for b in initboxes:
-        mappedpts = mapManyPoints(b,thresh,amp,rep,dr,previous1) #start below thresh
-    #now translate into outer approx
-    #then decide where we should subdivide
-
-def makeModel(sources,targets,thresholds,amplitudes,decayrates,repressors):
+def makeParameterArrays(sources,targets,thresholds,amplitudes,productionrates,decayrates,repressors):
     '''
-    See documentation for BooleanMapForDB.modelTrajectory.
+    Put model parameters into square arrays, where the column 
+    index indicates the source variable and the row index 
+    indicates the target variable.
+
+    The outputs thresh, amp, and rep are len(sources) x len(sources)
+    arrays with threshold values, amplitudes, and repressor 
+    identities filled in between the appropriate source-target 
+    interactions. All other array values are zero.
 
     '''
+    N = len(sources)
+    thresh = np.zeros((N,N))
+    amp = np.zeros((N,N))
+    for j,targ in enumerate(targets): #j is index of source
+        for k,t in enumerate(targ):
+            i = sources.index(t) #i is index of target
+            thresh[i,j] = thresholds[j][k]
+            amp[i,j] = amplitudes[j][k]
+    rep = np.zeros((N,N))
+    for r in repressors:
+        j = sources.index(r[0])
+        i = sources.index(r[1])
+        rep[i,j] = 1
     dr = np.array(decayrates)
-    thresh,amp,rep = BDB.makeParameterArrays(sources,targets,thresholds,amplitudes,repressors)
-    return thresh,amp,rep,dr
+    pr = np.array(productionrates)
+    return thresh, amp, rep.astype(int), dr, pr
 
+def runModel(thresh,amp,rep,dr,pr,maxvals):
+    walls = makeWalls(thresh,maxvals)
+    domains, focalpts = getDomainsAndFocalPoints(thresh,amp,rep,dr,pr,maxvals)
+    unidirwalls, unidirfps, whitewalls = identifyWhiteWalls(walls,domains,focalpts,dr)
+    next_threshs = getNextThresholds(unidirwalls, unidirfps, thresh)
+    wallvertices = constructVertices(unidirwalls)
+    mappedpts = []
+    allsteps = []
+    for w in wallvertices:
+        mp, mpa = mapManyPointsToMultipleHyperplanes(w,next_threshs,focalpts,dr)
+        mappedpts.append( mp )
+        allsteps.append( mpa )
+    return mappedpts, allsteps
 
 if __name__ == '__main__':
+    pass
     # # Bridget's example
     # sources = ['x','y1','y2','z']
     # targets = [['x','y1','z'],['y2'],['x','z'],['x']]
@@ -321,39 +336,7 @@ if __name__ == '__main__':
     # # for h in hyperplanes:
     # #     formattedh = ['({0:.3f}, {1:.3f})'.format(tup[0],tup[1]) for tup in h]
     # #     print(str(formattedh).translate(None, "'"))
-    # unidirectional, unidirfps, blackwalls, blackfps, whitewalls = identifyBlackWhiteWalls(hyperplanes,doms,fps,dr)
-    # print(unidirectional)
+    # unidirwalls, unidirfps, blackwalls, blackfps, whitewalls = identifyBlackWhiteWalls(hyperplanes,doms,fps,dr)
+    # print(unidirwalls)
     # print(blackwalls)
     # print(whitewalls)
-    # 3D example
-    sources = ['x','y','z']
-    targets = [['x','y','z'],['x','z'],['x']]
-    thresholds = [[0.25,0.5,0.75],[0.5,0.5],[0.5]]
-    amplitudes = [[0.6,1.0,1.0],[1.0,1.0],[1.0]]
-    decayrates = [1.0,0.5,0.5]
-    repressors = [('z','x')]
-    thresh,amp,rep,dr = makeModel(sources,targets,thresholds,amplitudes,decayrates,repressors)
-    maxvals = [2.0,3.0,4.0]
-    doms, fps = getDomainsAndFocalPoints(thresh,amp,rep,dr,maxvals)
-    print(len(doms))
-    # print(doms)
-    # print(fp)
-    hyperplanes = makeHyperplanes(thresh,maxvals)
-    print(len(hyperplanes))
-    # for h in hyperplanes:
-    #     formattedh = ['({0:.3f}, {1:.3f})'.format(tup[0],tup[1]) for tup in h]
-    #     print(str(formattedh).translate(None, "'"))
-    unidirectional, unidirfps, whitewalls = identifyWhiteWalls(hyperplanes,doms,fps,dr)
-    next_threshs = getNextThresholds(unidirectional, unidirfps, thresh)
-    print("White walls: {0}".format(whitewalls))
-    for k,u in enumerate(unidirectional):
-        formattedh = ['({0:.3f}, {1:.3f})'.format(tup[0],tup[1]) for tup in u]
-        print(str(formattedh).translate(None, "'"))
-        print("Focal point: {0}".format(unidirfps[k]))
-        print("Next hyperplanes: {0}".format(next_threshs[k]))
-    specpt = np.array([0.4,0.5,0.7])
-    fp = unidirfps[14]
-    nt = next_threshs[14]
-    shortest, allsteps = mapOnePointToMultipleHyperplanes(specpt,fp,nt,dr)
-    print(shortest)
-    print(allsteps)
---
+++ BooleanNetworks/VectorizedBooleanMapForDB_unittests.py
@@ -0,0 +1,44 @@
+import numpy as np
+import VectorizedBooleanMapForDB as VBD
+
+def xyz3DTest():
+    # 3D example
+    sources = ['x','y','z']
+    targets = [['x','y','z'],['x','z'],['x']]
+    thresholds = [[0.25,0.5,0.75],[0.5,0.5],[0.5]]
+    amplitudes = [[0.5,1.0,1.0],[1.0,1.0],[1.0]]
+    decayrates = [1.0,0.5,0.5]
+    repressors = [('z','x')]
+    productionrates = [0.1,0.1,0.1]
+    thresh,amp,rep,dr,pr = VBD.makeParameterArrays(sources,targets,thresholds,amplitudes,productionrates,decayrates,repressors)
+    maxvals = [2.0,3.0,4.0]
+    doms, fps = VBD.getDomainsAndFocalPoints(thresh,amp,rep,dr,pr,maxvals)
+    print("Number of domains: {0}".format(len(doms)))
+    walls = VBD.makeWalls(thresh,maxvals)
+    print("Number of walls: {0}".format(len(walls)))
+    # for w in walls:
+    #     formattedw = ['({0:.3f}, {1:.3f})'.format(tup[0],tup[1]) for tup in w]
+    #     print(str(formattedw).translate(None, "'"))
+    unidirwalls, unidirfps, whitewalls = VBD.identifyWhiteWalls(walls,doms,fps,dr)
+    next_threshs = VBD.getNextThresholds(unidirwalls, unidirfps, thresh)
+    print("White walls: {0}".format(whitewalls))
+    print("")
+    wallvertices = VBD.constructVertices(unidirwalls)
+    mappedpts = []
+    allsteps = []
+    for w in wallvertices:
+        mp, mpa = VBD.mapManyPointsToMultipleHyperplanes(w,next_threshs,fps,dr)
+        mappedpts.append( mp )
+        allsteps.append( mpa )
+    for k,u in enumerate(unidirwalls):
+        formattedh = ['({0:.3f}, {1:.3f})'.format(tup[0],tup[1]) for tup in u]
+        print("Wall: " + str(formattedh).translate(None, "'"))
+        print("Focal point: {0}".format(unidirfps[k]))
+        print("Next hyperplanes: {0}".format(next_threshs[k]))
+    #     print("Wall vertices: {0}".format(wallvertices[k]))
+    #     print("Next steps for vertices: {0}".format(mappedpts[k]))
+    #     print("All hyperplane steps for vertices: {0}".format(allsteps[k]))
+    #     print("")
+
+if __name__ == '__main__':
+    xyz3DTest()
