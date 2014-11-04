5acd8bb9ad3a24877e856b7d4178776b44eb11dd
Bree Cummins <breecummins@gmail.com>
2014-11-04 16:58:58 -0700

    counting orders because I suspect there will be memory issues when saving all the orders

---
+++ BooleanNetworks/ZaneCode/countallorders.m
@@ -0,0 +1,23 @@
+function orders = countallorders(g)
+
+	% recursive function
+	function orders = recurseunknown(g,unknownpairs,orders)
+		while ~isempty(unknownpairs)
+			up = unknownpairs(1,:);
+			disp(length(up));
+			for q = [up;fliplr(up)].'
+				g(q(1),q(2)) = 1;
+				g = transitiverelationships(g);
+				unknownpairs = unknownpairs(2:end,:);
+				orders = recurseunknown(g,unknownpairs,orders);
+			end
+		end
+		orders= orders+1;
+	end
+
+	unknownpairs = makeunknownpairs(g);
+	orders = 0;
+	orders = recurseunknown(g,unknownpairs,orders);
+	disp(length(orders))
+	save('orders.mat','orders')
+end
