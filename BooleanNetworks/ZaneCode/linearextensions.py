b544c455e506c8c3627c6bc99fbf4c9e4d49f912
Bree Cummins <breecummins@gmail.com>
2014-11-07 12:35:28 -0700

    have 2 working algorithms for getting linear extensions from a partial order.

--- BooleanNetworks/ZaneCode/linearextensions.py
+++ BooleanNetworks/ZaneCode/linearextensions.py
@@ -5,33 +5,17 @@ def checkpartialorder(order,kp):
                 return False
     return True
 
-def addpairs(k,order,kp):
-    pos = order.index(k)
-    for q in order[:pos]:
-        if [q,k] not in kp:
-            kp.append([q,k])
-    for q in order[pos+1:]:
-        if [k,q] not in kp:
-            kp.append([k,q])
-    return kp
+def flattenlessone(l):
+    if all(isinstance(el, list) for el in l) and not any(isinstance(a,list) for el in l for a in el):
+        return l
+    else:
+        flat = []
+        for el in l:
+            flat.append(el) if not isinstance(el,list) else flat.extend(flattenlessone(el))
+        return flat
 
 def insertall(k,order):
     return [order[:pos]+[k]+order[pos:] for pos in range(len(order)+1)]
-
-def recurseorder1(N,kp,order=[1]):
-    # This version returns duplicate copies in nested lists. Not ideal. See Graham's flat map to try and fix.
-    if len(order) == N:
-        return order
-    else:
-        orders = []
-        for k in range(1,N+1):
-            if k in order:
-                continue
-            else:
-                for o in insertall(k,order):
-                    if checkpartialorder(o,kp):
-                        orders.append(recurseorder1(N,kp,o))
-        return orders
 
 def recurseorder(N,kp,order=[1]):
     if len(order) == N:
@@ -42,15 +26,16 @@ def recurseorder(N,kp,order=[1]):
             if k in order:
                 continue
             else:
-                c = [r for o in insertall(k,order) for r in recurseorder(N,kp,o) if checkpartialorder(o,kp)]
-                print(c)
-                orders.append([q for q in c if q not in orders])
+                orders.extend(flattenlessone([recurseorder(N,kp,o) for o in insertall(k,order) if checkpartialorder(o,kp)]))
         return orders
-        # return [r for k in range(2,N+1) for o in insertall(k,order) for r in recurseorder(kp,N,o) if checkpartialorder(o,kp)]
+
+def linearextensions(N,kp):
+    l=recurseorder(N,kp)
+    return set([tuple(x) for x in l])
+
 
 if __name__ == '__main__':
-    l=recurseorder(3,[[1,2]])
-    print('------')
-    print(l)
-    # print(linearextensions(4,[[1,2],[1,3],[1,4],[2,4],[3,4]]))
+    # This algorithm is not nearly as good as linearextensions2.py.
+    print(linearextensions(3,[[1,2]]))
+    print(linearextensions(4,[[1,2],[1,3],[1,4],[2,4],[3,4]]))
 
