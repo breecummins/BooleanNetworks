2bd5510b3951e1339a7fdeb059182f4860d72e9e
Bree Cummins <breecummins@gmail.com>
2015-01-27 10:57:33 -0700

    added file where I changed name in previous commit

---
+++ BooleanNetworks/PatternMatching/fileparsers.py
@@ -0,0 +1,52 @@
+def parseOutEdges(fname='outEdges.txt'):
+    f=open(fname,'r')
+    outedges=[]
+    for l in f:
+        outedges.append(tuple([int(i) for i in l.split(' ')[1:-1]]))
+    return outedges
+
+def testParseOutEdges():
+    # compare the output file to the input file by eye
+    g=open('parsedoutedges.txt','w')
+    for oe in parseOutEdges():
+        g.write(str(oe)+'\n')
+    g.close()
+
+def parseWalls(fname='walls.txt'):
+    f=open(fname,'r')
+    walldomains=[]
+    for l in f:
+        L = filter(None,l.replace('[',' ').replace('x',' ').replace(']',' ').replace(',',' ').split(' ')[2:-1])
+        L = [float(n) for n in L]
+        T = []
+        for k in range(0,len(L)-1,2):
+            T.append(sum(L[k:k+2])/2) # thresholds are integers, regular domains end in .5
+        walldomains.append(tuple(T))
+    return walldomains
+
+def testParseWalls():
+    # compare the output file to the input file by eye
+    g=open('parsedwalls.txt','w')
+    for wd in parseWalls():
+        g.write(str(wd)+'\n')
+    g.close()
+
+def filterBoundaryWallsAndSteadyStates(outedges):
+    # get rid of boundary walls and steady states, because we shall assume that 
+    # searchable patterns have only extrema
+    # for use with patternmatch3
+    inedges=[tuple([j for j,o in enumerate(outedges) if i in o ]) for i in range(len(outedges))]
+    interiorinds=[]
+    interioroutedges=[]
+    for q,(o,i) in enumerate(zip(outedges,inedges)):
+        if i and (o!=(q,)): 
+            interiorinds.append(q)
+            interioroutedges.append([o])
+    for k,o in enumerate(interioroutedges):
+        newo = [j for j in o if j in interiorinds]
+        interioroutedges[k] = newo
+    return interiorinds,interioroutedges
+
+if __name__=='__main__':
+    # testParseOutEdges()
+    testParseWalls()
