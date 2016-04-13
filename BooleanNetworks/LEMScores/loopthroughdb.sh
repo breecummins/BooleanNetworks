b48965b5729c42d7647dce40d8b28fe22bef5e7e
Bree Cummins <breecummins@gmail.com>
2016-04-13 10:54:49 -0600

    corrected typo in file name

--- BooleanNetworks/LEMScores/loopthroughdb.sh
+++ BooleanNetworks/LEMScores/loopthroughdb.sh
@@ -4,5 +4,5 @@ cd /share/data/bcummins/DSGRN/software/Signatures/
 
 for i in $( ls ./databases/ ); do
 	NAME=${i%.*};
-	. /share/data/bcummins/BooleanNetworks/BooleanNetworks/LEMscores/calculateratios.sh ./databases/$i ./networks/$NAME.txt;
+	. /share/data/bcummins/BooleanNetworks/BooleanNetworks/LEMScores/calculateratios.sh ./databases/$i ./networks/$NAME.txt;
 done
