6ba482044c5b52b859a1aabeb36256abf08d1cbf
Bree Cummins <breecummins@gmail.com>
2016-04-13 10:59:36 -0600

    changed file path

--- BooleanNetworks/LEMScores/calculateratios.sh
+++ BooleanNetworks/LEMScores/calculateratios.sh
@@ -2,8 +2,7 @@
 # 1st argument is db name
 # 2nd argument is network spec file
 
-STABLEFC=$StableFCQuery.sh
-$STABLEFC $1 ./StableFCList.txt > /dev/null
+. /share/data/bcummins/BooleanNetworks/BooleanNetworks/LEMScores/StableFCQuery.sh $1 ./StableFCList.txt 
 STABLEFCS=`cut -d " " -f 1 StableFCList.txt | sort | uniq | wc -w`
 NODES=`dsgrn network $2 parameter | sed 's/[^0-9]*\([0-9]*\)[^0-9]*/\1/g'`
 echo $STABLEFCS / $NODES / bc <<< 'scale=2; $STABLEFCS/$NODES'
