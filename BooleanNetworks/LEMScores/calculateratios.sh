68c3ccdf65bb0544fa73cb72dd00f4d4a50b6c2b
Bree Cummins <breecummins@gmail.com>
2016-04-13 12:10:19 -0600

    adding calculateratios

--- BooleanNetworks/LEMScores/calculateratios.sh
+++ BooleanNetworks/LEMScores/calculateratios.sh
@@ -5,4 +5,4 @@
 . /share/data/bcummins/BooleanNetworks/BooleanNetworks/LEMScores/StableFCQuery.sh $1 ./StableFCList.txt 
 STABLEFCS=`cut -d " " -f 1 StableFCList.txt | sort | uniq | wc -w`
 NODES=`dsgrn network $2 parameter | sed 's/[^0-9]*\([0-9]*\)[^0-9]*/\1/g'`
-echo $STABLEFCS / $NODES / `bc <<< "scale=2; $STABLEFCS/$NODES"`
+echo $1 / $STABLEFCS / $NODES / `bc <<< "scale=2; $STABLEFCS/$NODES"`
